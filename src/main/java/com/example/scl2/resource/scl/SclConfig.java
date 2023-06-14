d2deafd0a3f18d11cd25d0cc5e63667a1e57b97a
Caio <90801189+CaioSF@users.noreply.github.com>
2023-06-14 10:02:53 -0300

    Finalização

--- src/main/java/com/example/scl2/resource/scl/SclConfig.java
+++ src/main/java/com/example/scl2/resource/scl/SclConfig.java
@@ -1,18 +1,15 @@
 package com.example.scl2.resource.scl;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class SclConfig implements WebMvcConfigurer {
-    @Override
+
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/*") // Defina o padrão de URL que deseja permitir
-                .allowedOrigins(" ") // Defina a origem permitida (ou "" para permitir todas)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Defina os métodos HTTP permitidos
-                .allowedHeaders(""); // Defina os cabeçalhos permitidos (ou "" para permitir todos)
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
     }
+
 }
